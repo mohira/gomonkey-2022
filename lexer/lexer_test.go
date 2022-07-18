47680e5b5f922e7670bfe63f18b995ad7342bdbe
mohira <shukatsudaison@gmail.com>
2022-07-19 00:55:19 +0900

    1.3 let ten = 10; もいける

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,6 +8,8 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `
 let five = 5;
+
+let ten = 10;
 `
 
 	tests := []struct {
@@ -19,6 +21,13 @@ let five = 5;
 		{token.IDENT, "five"},
 		{token.ASSIGN, "="},
 		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+
+		// let ten = 10;
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
 		{token.SEMICOLON, ";"},
 
 		{token.EOF, ""},
