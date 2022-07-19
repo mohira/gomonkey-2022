6ac392bb878104e917fb1c648a332d0c799f56cb
mohira <shukatsudaison@gmail.com>
2022-07-20 01:42:52 +0900

    トークンのポインタをゆっくり確かめる

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"gomonkey/ast"
 	"gomonkey/lexer"
 	"gomonkey/token"
@@ -19,6 +20,17 @@ func New(l *lexer.Lexer) *Parser {
 	p := &Parser{l: l}
 
 	// 2つトークンを読み込む。トークンの読み込みの雰囲気はLexerと同じだね。
+	// 2つ進めているので
+	//
+	// curToken
+	// |
+	// let age = 5;
+	//     |
+	//     peekToken
+	// ↓↓ こういう感じ
+	// 0) nil, nil
+	// 1) nil, LET
+	// 2) LET, IDENTIFIER
 	p.nextToken()
 	p.nextToken()
 
@@ -28,10 +40,16 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken // Lexer.position = Lexer.read_position と同じ関係
 	p.peekToken = p.l.NextToken()
+	fmt.Println(p)
+
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
 	program := &ast.Program{}
+
+	program.Statements = []ast.Statement{}
+
+	fmt.Println(p.curToken)
 
 	return program
 }
