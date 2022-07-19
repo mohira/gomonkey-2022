5211fad001bb94e33312d486022d4a403617fe3f
mohira <shukatsudaison@gmail.com>
2022-07-20 01:56:27 +0900

    let文の <identifier> のとこだけの解析は成功！ しかし、一歩が大きいので納得感は薄い！

--- parser/parser.go
+++ parser/parser.go
@@ -1,7 +1,6 @@
 package parser
 
 import (
-	"fmt"
 	"gomonkey/ast"
 	"gomonkey/lexer"
 	"gomonkey/token"
@@ -40,8 +39,6 @@ func New(l *lexer.Lexer) *Parser {
 func (p *Parser) nextToken() {
 	p.curToken = p.peekToken // Lexer.position = Lexer.read_position と同じ関係
 	p.peekToken = p.l.NextToken()
-	fmt.Println(p)
-
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -67,5 +64,47 @@ func (p *Parser) parseStatement() ast.Statement {
 		return p.parseLetStatement()
 	default:
 		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.curToken}
+
+	// expectPeekは <IDENT> なら 読み進めちゃっているよ！
+	if !p.expectPeek(token.IDENT) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+
+	// let <identifier> = <expression>
+	// なわけだから、次に <ASSIGN> じゃないのはおかしいよね
+	// → おかしな構文 → nil
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: セミコロンに遭遇するまで式を読み飛ばしてしまっている
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+	return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+	return p.peekToken.Type == t
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekTokenIs(t) {
+		p.nextToken() // ← 期待するTokenなら、読み進めちゃうぞ！！！！
+		return true
+	} else {
+		return false
 	}
 }
