3999743c8550b560adce86b5c7774a966c2d5428
mohira <shukatsudaison@gmail.com>
2022-07-20 23:24:58 +0900

    ルートノード定義。// Program ノードは、Statementでもないし、Expressionでもないですね。

--- ast/ast.go
+++ ast/ast.go
@@ -13,3 +13,16 @@ type Expression interface {
 	Node
 	expressionNode()
 }
+
+// Program ノードは、Statementでもないし、Expressionでもないですね。
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return "" // ルートノードしかない場合ってこと(文が一切ない"プログラム"のとき)
+	}
+}
