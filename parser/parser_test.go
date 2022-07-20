2a28253e57b6d2cb59213be28930b0fcb384ee99
mohira <shukatsudaison@gmail.com>
2022-07-21 00:11:47 +0900

    let文を検証するテストヘルパーメソッド

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,7 +1,9 @@
 package parser
 
 import (
+	"gomonkey/ast"
 	"gomonkey/lexer"
+	"gomonkey/token"
 	"testing"
 )
 
@@ -31,4 +33,47 @@ let foobar = 838383
 		t.Fatalf("3つのStatementからなるProgramじゃないのはおかしいよね。got = %q", len(program.Statements))
 	}
 
+	// ここからが let文 のパースって感じです。
+	// expressionは後回しにして、 <identifier> がちゃんと解析できているかを、まずは、調べていくぞ。
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+
+}
+
+func testLetStatement(t *testing.T, stmt ast.Statement, expectedName string) bool {
+	t.Helper()
+
+	// まずは LETトークンかどうかをちゃんと調べる
+	if stmt.TokenLiteral() != token.LET {
+		t.Errorf("stmt.TokenLiteral() が 'let` じゃないよ。 got = %q", stmt.TokenLiteral())
+		return false
+	}
+
+	// LETトークンをもっていても、LetStatement型じゃない可能性もあるのか
+	letStmt, ok := stmt.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("stmt が LetStatement じゃないよ。got=%Tl", stmt)
+		return false
+	}
+
+	// ここまで来たら ast.LetStatement 確定なので、素直に属性としての Name(つまり、<identifier>)を調べる
+	if letStmt.Name.Value != expectedName {
+		t.Errorf("letStmt.Name.Value が %q じゃないよ。got=%q", letStmt.Name.Value, expectedName)
+		return false
+	}
+
+	return true
 }
