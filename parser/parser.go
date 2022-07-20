5841bbf27e37a4818f4a9d1829261ae3606bd3a2
mohira <shukatsudaison@gmail.com>
2022-07-20 23:43:46 +0900

    1トークンずつみていくParserを考える

---
+++ parser/parser.go
@@ -0,0 +1,28 @@
+package parser
+
+import (
+	"gomonkey/lexer"
+	"gomonkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	// Lexerのときと同じ作戦。Lexerは「1文字」単位で、Parserは「1トークン」単位ですね。
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// 初期化時に2回読み進めておく
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
