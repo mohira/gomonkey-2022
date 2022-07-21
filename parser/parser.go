3ff0f0cc2efa1265f48c00f6a5f1cadcaf87bae0
mohira <shukatsudaison@gmail.com>
2022-07-22 00:23:49 +0900

    return文のパースでけた！ <expression>は後回しなので楽ちん！

--- parser/parser.go
+++ parser/parser.go
@@ -68,8 +68,6 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
-	case token.RETURN:
-		return p.parseReturnStatement()
 
 	default:
 		return nil
