9c50ae7635a1eefc087bfb4b34b5790766aea7a9
mohira <shukatsudaison@gmail.com>
2022-07-23 01:16:29 +0900

    return文のなんか消えてたw

--- parser/parser.go
+++ parser/parser.go
@@ -68,6 +68,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 
 	default:
 		return nil
