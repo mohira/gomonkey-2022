5c3256dc4edb93eeea74c1fc6e0d3cbee92f26c3
mohira <shukatsudaison@gmail.com>
2022-07-29 21:39:48 +0900

    謎の優先順位テーブルと、curTokenとpeekTokenの優先順位がわかるヘルパーメソッドを実装

--- parser/parser.go
+++ parser/parser.go
@@ -242,3 +242,34 @@ func (p *Parser) parsePrefixExpression() ast.Expression {
 
 	return prefixExpr
 }
+
+// 演算の優先順位テーブル
+var precedences = map[token.TokenType]int{
+	token.EQ:     EQUALS,
+	token.NOT_EQ: EQUALS,
+
+	token.LT: LESSGREATER,
+	token.GT: LESSGREATER,
+
+	token.PLUS:  SUM,
+	token.MINUS: SUM,
+
+	token.ASTERISK: PRODUCT,
+	token.SLASH:    PRODUCT,
+}
+
+func (p *Parser) curPrecedence() int {
+	if precedence, ok := precedences[p.curToken.Type]; ok {
+		return precedence
+	}
+
+	return LOWEST
+}
+
+func (p *Parser) peekPrecedence() int {
+	if precedence, ok := precedences[p.peekToken.Type]; ok {
+		return precedence
+	}
+
+	return LOWEST
+}
