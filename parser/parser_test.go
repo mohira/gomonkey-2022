84eee39a757d345bed12fb1c061b2e20e17a769b
mohira <shukatsudaison@gmail.com>
2022-10-11 15:54:25 +0900

    p.39 LetStatementのParseのテストを書いたところまで

---
+++ parser/parser_test.go
@@ -0,0 +1,62 @@
+package parser
+
+import (
+	"gomonkey/ast"
+	"gomonkey/lexer"
+	"testing"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+let x = 5;
+let y = 10;
+let foobar = 838383;
+`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	if program == nil {
+		t.Fatalf("ParseProgram() returned nil")
+	}
+
+	if len(program.Statements) != 3 {
+		t.Fatalf("program.Statements が 3文 じゃないよ. got=%d", len(program.Statements))
+	}
+
+	tests := []struct {
+		expectedIdentifier string
+	}{
+		{"x"},
+		{"y"},
+		{"foobar"},
+	}
+
+	for i, tt := range tests {
+		stmt := program.Statements[i]
+		if !testLetStatement(t, stmt, tt.expectedIdentifier) {
+			return
+		}
+	}
+}
+
+func testLetStatement(t *testing.T, stmt ast.Statement, expectedName string) bool {
+	t.Helper()
+
+	if stmt.TokenLiteral() != "let" {
+		t.Errorf("stmt.TokenLiteral not 'let'. got=%q", stmt.TokenLiteral())
+		return false
+	}
+
+	letStmt, ok := stmt.(*ast.LetStatement)
+	if !ok {
+		t.Errorf("stmt が *ast.LetStatement じゃないよ。got=%T", letStmt)
+		return false
+	}
+
+	if letStmt.Name.Value != expectedName {
+		t.Errorf("letStmt.Name.Value が '%s' じゃないよ。got=%s", expectedName, letStmt.Name.Value)
+		return false
+	}
+	return true
+}
