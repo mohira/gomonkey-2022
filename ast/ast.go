0b9e3253fb58931d8ff9105f9a032e795814fcaa
mohira <shukatsudaison@gmail.com>
2022-10-17 22:35:25 +0900

    Ifノードの定義

--- ast/ast.go
+++ ast/ast.go
@@ -235,3 +235,34 @@ func (b *Boolean) TokenLiteral() string {
 func (b *Boolean) String() string {
 	return b.Token.Literal
 }
+
+type IfExpression struct {
+	Token       token.Token // token.IF
+	Condition   Expression
+	Consequence *BlockStatement
+	Alternative *BlockStatement
+}
+
+func (ie *IfExpression) TokenLiteral() string {
+	return ie.Token.Literal
+}
+
+func (ie *IfExpression) expressionNode() {
+	panic("implement me")
+}
+
+func (ie *IfExpression) String() string {
+	var out strings.Builder
+	// if (cond) { consequence } else { alternative }
+	out.WriteString("if")
+	out.WriteString(ie.Condition.String())
+	out.WriteString(" ")
+	out.WriteString(ie.Consequence.String())
+
+	if ie.Alternative != nil {
+		out.WriteString("else ")
+		out.WriteString(ie.Alternative.String())
+	}
+
+	return out.String()
+}
