424fa4507616e7e0a84d152b1397c4befbdd8788
mohira <shukatsudaison@gmail.com>
2022-10-19 19:54:03 +0900

    2.8.4: 関数リテラルのASTノードを定義

--- ast/ast.go
+++ ast/ast.go
@@ -293,3 +293,35 @@ func (bs *BlockStatement) String() string {
 
 	return out.String()
 }
+
+type FunctionLiteral struct {
+	Token      token.Token // token.FUNCTION
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {
+	panic("implement me")
+}
+
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	// fn( <parameters> ) { <block_statement> }
+	var out strings.Builder
+
+	var params []string
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
