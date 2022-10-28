f5f7eb2b8d16883e2b0cf8ab05fd56209ebceb40
mohira <shukatsudaison@gmail.com>
2022-10-28 19:46:02 +0900

    2.8.7 fix: FunctionLiteral.String()がたりてなかった。

--- ast/ast.go
+++ ast/ast.go
@@ -321,7 +321,9 @@ func (fl *FunctionLiteral) String() string {
 	out.WriteString("(")
 	out.WriteString(strings.Join(params, ", "))
 	out.WriteString(") ")
+	out.WriteString("{ ")
 	out.WriteString(fl.Body.String())
+	out.WriteString(" }")
 
 	return out.String()
 }
