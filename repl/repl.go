cab9821c274e6e0727e6ee5a8a843e366e15c7a1
mohira <shukatsudaison@gmail.com>
2022-10-28 19:41:37 +0900

    2.8.7 RPPL

--- repl/repl.go
+++ repl/repl.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"gomonkey/lexer"
-	"gomonkey/token"
+	"gomonkey/parser"
 	"io"
 )
 
@@ -24,9 +24,23 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		_, _ = io.WriteString(out, program.String())
+		_, _ = io.WriteString(out, "\n")
+
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		_, _ = io.WriteString(out, "\t"+msg+"n")
 	}
 }
