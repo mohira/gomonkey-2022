cc9c39594ae998c5e830b3586ee46d1f8bbb59fb
mohira <shukatsudaison@gmail.com>
2022-10-30 16:09:30 +0900

    3.4.2 Integerオブジェクトを定義。

--- object/object.go
+++ object/object.go
@@ -1,8 +1,26 @@
 package object
 
+import "fmt"
+
 type Type string
+
+const (
+	IntegerObj = "INTEGER"
+)
 
 type Object interface {
 	Type() Type
 	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Type() Type {
+	return IntegerObj
+}
+
+func (i *Integer) Inspect() string {
+	return fmt.Sprintf("%d", i.Value)
 }
