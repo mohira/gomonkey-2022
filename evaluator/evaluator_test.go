2b61906bb7e1f0d5466c4f38d0b99e6189581f6c
mohira <shukatsudaison@gmail.com>
2022-11-01 19:30:33 +0900

    3.6 複数の文がくると落ちることを確認した

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -183,6 +183,8 @@ func TestIfElseExpressions(t *testing.T) {
 		{"if (1 > 2) { 10 }", nil},
 		{"if (1 > 2) { 10 } else { 20 }", 20},
 		{"if (1 < 2) { 10 } else { 20 }", 10},
+
+		{"if (1 < 2) { 10; 20; } else { 30 }", 20},
 	}
 
 	for _, tt := range tests {
