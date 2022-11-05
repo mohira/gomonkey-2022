9cc54d955b88e4d0346a40a9dd49f0fcc44fd23e
mohira <shukatsudaison@gmail.com>
2022-11-05 19:11:22 +0900

    3.9 let文で実現したいことをテストに書いた

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -320,3 +320,26 @@ func TestErrorHandling(t *testing.T) {
 		})
 	}
 }
+
+func TestLetStatements(t *testing.T) {
+	// let文の評価と、ちゃんと名前で値が保存されているかを確認するわけだよ
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+
+		// 再代入ありなケース
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			evaluated := testEval(tt.input)
+
+			testIntegerObject(t, evaluated, tt.expected)
+		})
+	}
+}
