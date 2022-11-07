843450f47881260598e0cf36d4939f8081538735
mohira <shukatsudaison@gmail.com>
2022-11-07 22:51:32 +0900

    4.3.1: len関数実装！ そして、GREEN！

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -1,11 +1,23 @@
 package evaluator
 
-import "gomonkey/object"
+import (
+	"gomonkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
-	"len": &object.Builtin{
+	"len": {
 		Fn: func(args ...object.Object) object.Object {
-			return NULL
+			// argsは1個 && STRING => len(STRING) // 配列とかハッシュマップに対してのlenはまたあとでな！
+			if len(args) != 1 {
+				return newError("argument error: wrong number of arguments (given %d, expected %d)", len(args), 1)
+			}
+
+			arg, ok := args[0].(*object.String)
+			if !ok {
+				return newError("argument to `len` not supported, got %s", args[0].Type())
+			}
+
+			return &object.Integer{Value: int64(len(arg.Value))}
 		},
 	},
 }
