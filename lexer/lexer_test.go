8e0a60f06748b65bc0a915dba0976a0be76dfc98
mohira <shukatsudaison@gmail.com>
2022-11-09 20:40:59 +0900

    4.4.1 [ と ] の字句解析おっけ〜

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -241,3 +241,34 @@ bar"
 	}
 
 }
+
+func TestNextToken_配列に関係するやつ(t *testing.T) {
+	input := `
+[1, 2];
+`
+	tests := []struct {
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		{token.LBRACKET, "["},
+		{token.INT, "1"},
+		{token.COMMA, ","},
+		{token.INT, "2"},
+		{token.RBRACKET, "]"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
