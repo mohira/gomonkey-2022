e72f55ca94cf64f1d3b76afba49b3f1b0b2e6ace
mohira <shukatsudaison@gmail.com>
2022-11-14 21:57:42 +0900

    4.5.1 `:`を字句解析できるようにした

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -272,3 +272,30 @@ func TestNextToken_配列に関係するやつ(t *testing.T) {
 		}
 	}
 }
+func TestNextToken_ハッシュリテラルに関係するやつ(t *testing.T) {
+	input := `{"foo": "bar"}`
+	tests := []struct {
+		expectedType    token.Type
+		expectedLiteral string
+	}{
+		{token.LBRACE, "{"},
+		{token.STRING, "foo"},
+		{token.COLON, ":"},
+		{token.STRING, "bar"},
+		{token.RBRACE, "}"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
