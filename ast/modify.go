30209b32b4670e07d077d4287d249d274e1747b6
mohira <shukatsudaison@gmail.com>
2022-11-20 18:29:39 +0900

    A.4.1.1 ast.Modifyの最初のテストを通した！

---
+++ ast/modify.go
@@ -0,0 +1,18 @@
+package ast
+
+type ModifyFunc func(Node) Node
+
+func Modify(node Node, modifier ModifyFunc) Node {
+	switch node := node.(type) {
+	case *Program:
+		for i, statement := range node.Statements {
+			// MEMO: エラー処理は！？
+			node.Statements[i], _ = Modify(statement, modifier).(Statement)
+		}
+	case *ExpressionStatement:
+		// MEMO: エラー処理は！？
+		node.Expression, _ = Modify(node.Expression, modifier).(Expression)
+	}
+
+	return modifier(node)
+}
